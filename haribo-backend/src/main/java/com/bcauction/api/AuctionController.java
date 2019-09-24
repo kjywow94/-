3421c7a3ab7ef0d154b8ddf6b4c68856e84e8f74
NobleTuna <lyy7661@naver.com>
2019-09-24 17:08:02 +0900

    이미지 추가기능 완료

--- haribo-backend/src/main/java/com/bcauction/api/AuctionController.java
+++ haribo-backend/src/main/java/com/bcauction/api/AuctionController.java
@@ -4,6 +4,7 @@ import com.bcauction.application.IAuctionContractService;
 import com.bcauction.application.IAuctionService;
 import com.bcauction.domain.Auction;
 import com.bcauction.domain.AuctionInfo;
+import com.bcauction.domain.AuctionWithImg;
 import com.bcauction.domain.Bid;
 import com.bcauction.domain.exception.ApplicationException;
 import com.bcauction.domain.exception.EmptyListException;
@@ -20,16 +21,14 @@ import java.util.List;
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
-public class AuctionController
-{
+public class AuctionController {
 	public static final Logger logger = LoggerFactory.getLogger(AuctionController.class);
 
 	private IAuctionService auctionService;
 	private IAuctionContractService auctionContractService;
 
 	@Autowired
-	public AuctionController(IAuctionService auctionService,
-	                         IAuctionContractService auctionContractService) {
+	public AuctionController(IAuctionService auctionService, IAuctionContractService auctionContractService) {
 		Assert.notNull(auctionService, "auctionService 개체가 반드시 필요!");
 		Assert.notNull(auctionContractService, "auctionContractService 개체가 반드시 필요!");
 
@@ -40,31 +39,31 @@ public class AuctionController
 	@RequestMapping(value = "/auctions", method = RequestMethod.POST)
 	public Auction 생성(@RequestBody Auction auction) {
 		Auction 경매 = auctionService.생성(auction);
-		if( 경매 == null )
+		if (경매 == null)
 			throw new ApplicationException("경매 정보를 입력할 수 없습니다!");
 
 		return 경매;
 	}
 
 	@RequestMapping(value = "/auctions", method = RequestMethod.GET)
-	public List<Auction> 목록조회() {
+	public List<AuctionWithImg> 목록조회() {
 		List<Auction> 목록 = auctionService.경매목록조회();
 
-		if (목록 == null || 목록.isEmpty() )
+		if (목록 == null || 목록.isEmpty())
 			throw new EmptyListException("NO DATA");
 
-		return 목록;
+		return auctionService.auctionList(목록);
 	}
 
 	@RequestMapping(value = "/auctions/{id}", method = RequestMethod.GET)
 	public AuctionInfo 조회(@PathVariable long id) {
 		Auction 경매 = this.auctionService.조회(id);
-		if (경매 == null){
+		if (경매 == null) {
 			logger.error("NOT FOUND AUCTION: ", id);
 			throw new NotFoundException(id + " 해당 경매를 찾을 수 없습니다.");
 		}
 		AuctionInfo 경매정보 = this.auctionContractService.경매정보조회(경매.get컨트랙트주소());
-		if(경매정보 == null){
+		if (경매정보 == null) {
 			throw new NotFoundException(id + " 해당 경매 컨트랙트를 찾을 수 없습니다.");
 		}
 		경매정보.set경매시작시간(경매.get시작일시());
@@ -79,7 +78,7 @@ public class AuctionController
 	}
 
 	@RequestMapping(value = "/auctions/{aid}/by/{mid}", method = RequestMethod.PUT)
-	public Auction 경매종료(@PathVariable long aid, @PathVariable long mid) { //mid = 최고가 입찰자 id
+	public Auction 경매종료(@PathVariable long aid, @PathVariable long mid) { // mid = 최고가 입찰자 id
 		return this.auctionService.경매종료(aid, mid);
 	}
 
@@ -89,19 +88,16 @@ public class AuctionController
 	}
 
 	/**
-	 * 협업과제
-	 * 협업과제
-	 * week. 4-7
-	 * mission. 3
-	 * Req. 1-2
+	 * 협업과제 협업과제 week. 4-7 mission. 3 Req. 1-2
 	 */
 	@RequestMapping(value = "/auctions/owner/{id}", method = RequestMethod.GET)
-	public List<Auction> 사용자경매목록조회(@PathVariable int id){
-		// TODO 
+	public List<AuctionWithImg> 사용자경매목록조회(@PathVariable int id) {
+		// TODO
 		List<Auction> result = new ArrayList<Auction>();
-		for(Auction a : auctionService.경매목록조회())
-			if(a.get경매생성자id() == id)
+		for (Auction a : auctionService.경매목록조회())
+			if (a.get경매생성자id() == id)
 				result.add(a);
-		return result;
+
+		return auctionService.auctionList(result);
 	}
 }
--- haribo-backend/src/main/java/com/bcauction/application/impl/AuctionService.java
+++ haribo-backend/src/main/java/com/bcauction/application/impl/AuctionService.java
@@ -4,7 +4,10 @@ import com.bcauction.application.IAuctionContractService;
 import com.bcauction.application.IAuctionService;
 import com.bcauction.application.IFabricService;
 import com.bcauction.domain.Auction;
+import com.bcauction.domain.AuctionWithImg;
 import com.bcauction.domain.Bid;
+import com.bcauction.domain.DigitalWork;
+import com.bcauction.domain.DigitalWorkWithImg;
 import com.bcauction.domain.Ownership;
 import com.bcauction.domain.exception.ApplicationException;
 import com.bcauction.domain.exception.NotFoundException;
@@ -15,13 +18,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.math.BigInteger;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 
 @Service
-public class AuctionService implements IAuctionService
-{
+public class AuctionService implements IAuctionService {
 	public static final Logger logger = LoggerFactory.getLogger(AuctionService.class);
 
 	private IAuctionContractService auctionContractService;
@@ -30,9 +37,8 @@ public class AuctionService implements IAuctionService
 	private IBidRepository bidRepository;
 
 	@Autowired
-	public AuctionService(IAuctionContractService auctionContractService,
-						  IFabricService fabricService,
-							IAuctionRepository auctionRepository, IBidRepository bidRepository) {
+	public AuctionService(IAuctionContractService auctionContractService, IFabricService fabricService,
+			IAuctionRepository auctionRepository, IBidRepository bidRepository) {
 		this.auctionContractService = auctionContractService;
 		this.fabricService = fabricService;
 		this.auctionRepository = auctionRepository;
@@ -56,12 +62,18 @@ public class AuctionService implements IAuctionService
 
 	@Override
 	public Auction 생성(final Auction 경매) {
-		if(경매.get시작일시() == null) return null;
-		if(경매.get종료일시() == null) return null;
-		if(경매.get경매생성자id() == 0) return null;
-		if(경매.get경매작품id() == 0) return null;
-		if(경매.get컨트랙트주소() == null) return null;
-		if(경매.get최저가() == null) return null;
+		if (경매.get시작일시() == null)
+			return null;
+		if (경매.get종료일시() == null)
+			return null;
+		if (경매.get경매생성자id() == 0)
+			return null;
+		if (경매.get경매작품id() == 0)
+			return null;
+		if (경매.get컨트랙트주소() == null)
+			return null;
+		if (경매.get최저가() == null)
+			return null;
 
 		경매.set생성일시(LocalDateTime.now());
 		long id = this.auctionRepository.생성(경매);
@@ -75,62 +87,87 @@ public class AuctionService implements IAuctionService
 	}
 
 	@Override
-	public Bid 낙찰(final long 경매id, final long 낙찰자id, final BigInteger 입찰최고가)
-	{
+	public Bid 낙찰(final long 경매id, final long 낙찰자id, final BigInteger 입찰최고가) {
 		int affected = this.bidRepository.수정(경매id, 낙찰자id, 입찰최고가);
-		if(affected == 0)
+		if (affected == 0)
 			return null;
 
 		return this.bidRepository.조회(경매id, 낙찰자id, 입찰최고가);
 	}
 
 	/**
-	 * 프론트엔드에서 스마트 컨트랙트의 경매종료(endAuction) 함수 직접 호출 후
-	 * 백엔드에 경매 상태 동기화를 위해 호출되는 메소드
+	 * 프론트엔드에서 스마트 컨트랙트의 경매종료(endAuction) 함수 직접 호출 후 백엔드에 경매 상태 동기화를 위해 호출되는 메소드
+	 * 
 	 * @param 경매id
 	 * @param 회원id
-	 * @return Auction
-	 * 1. 해당 경매의 상태가 E(ended)로 바뀌고,
-	 * 2. 입찰 정보 중 최고 입찰 정보를 '낙찰'로 업데이트해야 한다.
-	 * 3. 데이터베이스의 소유권정보를 업데이트 한다.
-	 * 4. 패브릭 상에도 소유권 이전 정보가 추가되어야 한다.
-	 * 5. 업데이트 된 경매 정보를 반환한다.
-	 * */
+	 * @return Auction 1. 해당 경매의 상태가 E(ended)로 바뀌고, 2. 입찰 정보 중 최고 입찰 정보를 '낙찰'로
+	 *         업데이트해야 한다. 3. 데이터베이스의 소유권정보를 업데이트 한다. 4. 패브릭 상에도 소유권 이전 정보가 추가되어야 한다.
+	 *         5. 업데이트 된 경매 정보를 반환한다.
+	 */
 	@Override
-	public Auction 경매종료(final long 경매id, final long 회원id)
-	{
+	public Auction 경매종료(final long 경매id, final long 회원id) {
 		// TODO
 		Bid bid = this.bidRepository.최고입잘조회(경매id);
 		this.bidRepository.수정(경매id, bid.get경매참여자id(), bid.get입찰금액().toBigInteger());
-		
+
 		Auction auction = this.auctionRepository.조회(경매id);
 		auction.set상태("E");
 		this.auctionRepository.수정(auction);
 		// 소유권 이전 호출 추가 필요
-		fabricService.소유권이전( auction.get경매생성자id(), bid.get경매참여자id(), auction.get경매작품id());
+		fabricService.소유권이전(auction.get경매생성자id(), bid.get경매참여자id(), auction.get경매작품id());
 		return auction;
 	}
 
 	/**
-	 * 프론트엔드에서 스마트 컨트랙트의 경매취소(cancelAuction) 함수 직접 호출 후
-	 * 백엔드에 경매 상태 동기화를 위해 호출되는 메소드
+	 * 프론트엔드에서 스마트 컨트랙트의 경매취소(cancelAuction) 함수 직접 호출 후 백엔드에 경매 상태 동기화를 위해 호출되는 메소드
+	 * 
 	 * @param 경매id
 	 * @param 회원id
-	 * @return Auction
-	 * 1. 해당 경매의 상태와(C,canceled) 종료일시를 업데이트 한다.
-	 * 2. 입찰 정보 중 최고 입찰 정보를 '낙찰'로 업데이트해야 한다.
-	 * 3. 업데이트 된 경매 정보를 반환한다.
-	 * */
+	 * @return Auction 1. 해당 경매의 상태와(C,canceled) 종료일시를 업데이트 한다. 2. 입찰 정보 중 최고 입찰 정보를
+	 *         '낙찰'로 업데이트해야 한다. 3. 업데이트 된 경매 정보를 반환한다.
+	 */
 	@Override
-	public Auction 경매취소(final long 경매id, final long 회원id)
-	{
+	public Auction 경매취소(final long 경매id, final long 회원id) {
 		// TODO
-		
+
 		Auction auction = this.auctionRepository.조회(경매id);
 		auction.set상태("C");
 		auction.set종료일시(LocalDateTime.now());
 		this.auctionRepository.수정(auction);
-		
+
 		return auction;
+	}
+
+	@Override
+	public List<AuctionWithImg> auctionList(List<Auction> 목록) {
+		List<AuctionWithImg> listWithImg = new ArrayList<>();
+
+		for (Auction auction : 목록) {
+
+			try {
+				File f = new File("worksImage/" + auction.get경매작품id());
+				FileInputStream fis = new FileInputStream(f);
+				byte byteArray[] = new byte[(int) f.length()];
+				fis.read(byteArray);
+				String encodeImg = "data:image/" + auction.getId() + ";base64, "
+						+ Base64.getEncoder().encodeToString(byteArray);
+				AuctionWithImg awi = new AuctionWithImg(encodeImg);
+				fis.close();
+				awi.setId(auction.getId());
+				awi.set경매생성자id(auction.get경매생성자id());
+				awi.set경매작품id(auction.get경매작품id());
+				awi.set상태(auction.get상태());
+				awi.set생성일시(auction.get생성일시());
+				awi.set시작일시(auction.get시작일시());
+				awi.set종료일시(auction.get종료일시());
+				awi.set최저가(auction.get최저가());
+				awi.set컨트랙트주소(auction.get컨트랙트주소());
+				listWithImg.add(awi);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return listWithImg;
 	}
 }
