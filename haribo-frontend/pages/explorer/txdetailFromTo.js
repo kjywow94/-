a396dcf9b836a6f35480f4d07bbeb94673b0c856
kjy_wow <kjy00707@gmail.com>
2019-09-25 11:05:13 +0900

    이더 숫자 변형중

--- haribo-frontend/pages/explorer/txdetailFromTo.js
+++ haribo-frontend/pages/explorer/txdetailFromTo.js
@@ -51,7 +51,7 @@ var explorerTxDetailFromToView = Vue.component('ExplorerTxDetailFromToView', {
                             <td v-else scope="row"><router-link :to="{ name: 'explorer.tx.detail.fromto', params: { address: tran.from }}" class="block-number">{{ tran.from | truncate(15) }}</router-link></td>
                             <td v-if="fa.id === tran.to" scope="row"> {{ tran.to | truncate(15) }}</td>
                             <td v-else scope="row"><router-link :to="{ name: 'explorer.tx.detail.fromto', params: { address: tran.to }}" class="block-number">{{ tran.to | truncate(15) }}</router-link></td>
-                            <td scope="row"> 111 ether</td>
+                            <td scope="row"> {{ tran.amount }} ether</td>
                             <td scope="row"> {{ tran.gas}} ether </td>
                         </tr>
                     </tbody>
@@ -83,6 +83,7 @@ var explorerTxDetailFromToView = Vue.component('ExplorerTxDetailFromToView', {
             /**
              * 트랜잭션 해시값으로 트랜잭션 정보를 가져옵니다.
              */
+
             var ta = (address) => {
                 this.fa = address;
                 var idx = 0;
@@ -107,8 +108,10 @@ var explorerTxDetailFromToView = Vue.component('ExplorerTxDetailFromToView', {
                     this.number = String(this.fa.trans[i].gas);
                     this.fa.trans[i].gas = web3.utils.fromWei(this.number, "ether");
                   
-                    // this.value = String(this.fa.trans[i].amount);
-                    // this.fa.trans[i].amount = web3.utils.fromWei(this.value, "ether");
+                    console.log(this.fa.trans[i].amount);
+                    
+                    this.value = String(this.fa.trans[i].amount);
+                    this.fa.trans[i].amount = web3.utils.fromWei(this.value, "ether");
 
                     var len = this.fa.trans.length;
                     var num = len -1;
